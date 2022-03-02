ad385b6c58adb022cac136653fab3ef3de944c5b
Agustin Cicchitti <agucichii@gmail.com>
2022-03-02 10:44:33 +0100

    Escritura método de lectura de csv y guardado acciones Commit #4

--- src/main/java/com/sinensia/principal/Main.java
+++ src/main/java/com/sinensia/principal/Main.java
@@ -1,9 +1,28 @@
 package com.sinensia.principal;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.sinensia.entidades.Acciones;
+import com.sinensia.servicios.Metodos;
+
 public class Main {
 
+	private static final String FILE_NAME = "GOOG.csv";
+	private static final String FILE_URL = "https://query1.finance.yahoo.com/v7/finance/download/GOOG?";
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Metodos met = new Metodos();
+
+		try {
+			met.DescargaFichero(FILE_URL, FILE_NAME);
+			Acciones valores = met.LecturaFichero("C:\\Users\\aguci\\eclipse-workspace\\accionesproject\\", FILE_NAME);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
